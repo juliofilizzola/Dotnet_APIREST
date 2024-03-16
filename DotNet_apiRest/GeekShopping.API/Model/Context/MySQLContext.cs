9f0ca6cda1aa50c5db7b83b1a15463fe01e9cf69
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-03-16 11:53:49 -0300

    create migration

---
+++ DotNet_apiRest/GeekShopping.API/Model/Context/MySQLContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.API.Model.Context;
+
+public class MySqlContext(DbContextOptions<MySqlContext> options) : DbContext(options) {
+    public DbSet<Product> Products { get; set; }
+}
---
+++ DotNet_apiRest/GeekShopping.API/Model/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using GeekShopping.API.Model.Base;
+
+namespace GeekShopping.API.Model;
+
+[Table("product")]
+public class Product: BaseEntity {
+    [Column("name")]
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [Column("price")]
+    [Required]
+    [Range(1, 1000000)]
+    public decimal Price { get; set; }
+
+    [Column("description")]
+    [StringLength(5000)]
+    public string Description { get; set; }
+
+    [Column("category_name")]
+    [Required]
+    [StringLength(50)]
+    public string CategoryName { get; set; }
+
+    [Column("image_url")]
+    [StringLength(300)]
+    public string ImageUrl { get; set; }
+}
